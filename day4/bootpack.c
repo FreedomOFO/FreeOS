292831706447bc16081647de958959bbb08c2669
FreedomOFO <freedomofo@gmail.com>
2016-04-09 14:00:13 +0800

    day4完整版~
    满满成就感的一天！果然还是看得见的东西做起来比较开森啊

--- day4/bootpack.c
+++ day4/bootpack.c
@@ -9,22 +9,23 @@ void boxfill8(unsigned char *vram,int xsize,unsigned char c,int x0,int y0,int x1
 void init_palette(void);
 void set_palette(int start,int end,unsigned char *rgb);
 
-#define BLACK 		0;
-#define	LIGHTRED	1;
-#define LIGHTGREEN	2;
-#define LIGHTYELLOW	3;
-#define	LIGHTBLUE	4;
-#define LIGHTPUPPLE 5;
-#define	SLIGHTBLUE	6;
-#define	WHITE		7;
-#define	LIGHTGREY	8;
-#define	DARKRED		9;
-#define	DARKGREEN	10;
-#define	DARKYELLOW	11;
-#define	DARKGGREEN	12;
-#define	DARKPUPPLE	13;
-#define	SDARKBLUE	14;
-#define	DARKGREY	15;
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
+
 
 void HariMain(void)
 {
@@ -32,11 +33,11 @@ void HariMain(void)
 	
 	init_palette();				//设定调色板
 	
-	p=(char *)0xa0000;			//指定地址
+	p=(char *) 0xa0000;		//指定地址
 	
-	boxfill8(p,320,BLACK,20,20,120,120);
-	boxfill8(p,320,LIGHTGREEN,20,20,120,120);
-	boxfill8(p,320,LIGHTBLUE,20,20,120,120);
+	boxfill8(p,320,COL8_008400	,20,20,120,120);
+	boxfill8(p,320,COL8_00FF00,70,50,170,150);
+	boxfill8(p,320,COL8_0000FF,120,80,220,180);
 	for(;;){
 		io_hlt();
 		}
